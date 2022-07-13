5924245c99d52bb2f613f9d87f8c72d9a47d7154
unknown <denberdnikov@gmail.com>
2022-07-13 15:48:41 +0300

    Add database connection

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BOT_TOKEN = "5451974963:AAEzIsjkgI6jjyyeud3VDznrEp29UW6qv_8"
 #user = "rhkaahzcelvipx"
 #password = "d5568e9708d54bdc54ea0f946e138a2db6c86595e4ad1c9b776ea4127b830306"
 APP_URL = "https://letibottest.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://rhkaahzcelvipx:d5568e9708d54bdc54ea0f946e138a2db6c86595e4ad1c9b776ea4127b830306@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dan9ja24q5a5v4"
+DB_URI = "postgres://yajqxvyyqixgdo:538043a80ed5bfd06242b430293e548df2ff183d7b08fa5205f5779cdb46294d@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/d7udqrtl1o2p6q"
